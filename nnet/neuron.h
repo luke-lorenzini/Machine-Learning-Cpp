0d72761501b7dcfa8c6d4b85e2aee56107787fb9
Luke Lorenzini <luke.lorenzini@gmail.com>
2018-08-01 14:16:33 +0100

    Add neuron.h

---
+++ nnet/neuron.h
@@ -0,0 +1,108 @@
+#pragma once
+#define _USE_FIXED_RAND
+
+typedef double type_t;
+
+class neuron
+{
+public:
+	neuron(int input, int output);
+	~neuron();
+
+	void fwd(std::vector<type_t> &x);
+	void bkwd(std::vector<type_t> &delta_in);
+	void accm(std::vector<type_t> &x);
+	void updt(int samples);
+	void set_error();
+
+	std::vector<type_t>& get_y();
+	std::vector<type_t>& get_delta();
+	std::vector<type_t>& get_W();
+	std::vector<type_t>& get_error();
+
+	static void matrix_sub(const std::vector<type_t> &a, const std::vector<type_t> &b, std::vector<type_t> &res, int M, int N);
+	
+private:
+	const double ALPHA = 0.8;
+	const int COLS = 1;
+
+	std::random_device rd;
+
+	int x_rows;
+	int x_cols;
+
+	int t_x_rows;
+	int t_x_cols;
+	int t_x_size;
+	std::vector<type_t> t_x;
+
+	int W_rows;
+	int W_cols;
+	int W_size;
+	std::vector<type_t> W;
+
+	int W_Trans_rows;
+	int W_Trans_cols;
+	int W_Trans_size;
+	std::vector<type_t> W_Trans;
+
+	int delta_in_rows;
+	int delta_in_cols;
+
+	int z_rows;
+	int z_cols;
+	int z_size;
+	std::vector<type_t> z;
+
+	int y_rows;
+	int y_cols;
+	int y_size;
+	std::vector<type_t> y;
+
+	int t_y_rows;
+	int t_y_cols;
+	int t_y_size;
+	std::vector<type_t> t_y;
+
+	int t_e0_rows;
+	int t_e0_cols;
+	int t_e0_size;
+	std::vector<type_t> t_e0;
+
+	int error_rows;
+	int error_cols;
+	int error_size;
+	std::vector<type_t> error;
+
+	int ones_rows;
+	int ones_cols;
+	int ones_size;
+	std::vector<type_t> ones;
+
+	int delta_W_rows;
+	int delta_W_cols;
+	int delta_W_size;
+	std::vector<type_t> delta_W;
+
+	int t_delta_W_rows;
+	int t_delta_W_cols;
+	int t_delta_W_size;
+	std::vector<type_t> t_delta_W;
+
+	int delta_rows;
+	int delta_cols;
+	int delta_size;
+	std::vector<type_t> delta;
+
+	int t_delta_out_rows;
+	int t_delta_out_cols;
+	int t_delta_out_size;
+	std::vector<type_t> t_delta_out;
+
+	void init_rand_real(int size, std::vector<type_t> &vect);
+	void init_zeros(int size, std::vector<type_t> &vect);
+	void init_ones(int size, std::vector<type_t> &vect);
+
+	void activate(int actFunct);
+	void activate_der(int actFunct);
+};
