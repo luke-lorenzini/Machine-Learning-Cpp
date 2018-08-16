dc3e04b08c341b3be75be7d551355fd42e9c1d97
Luke Lorenzini <luke.lorenzini@gmail.com>
2018-08-16 08:04:45 +0100

    Updates for various modes of operation

--- nnet/source.cpp
+++ nnet/source.cpp
@@ -1,35 +1,51 @@
 #include "stdafx.h"
 #include "file.h"
 #include "gpu.h"
+#include "logistic_neuron.h"
 #include "neuron.h"
+#include "nnet_math.h"
+#include "softmax_neuron.h"
+#include "relu_neuron.h"
+#include "tanh_neuron.h"
 
-#define _USEREALDATA
+typedef double type_t;
+
+#define MNIST	0
+#define IRIS	4
+#define CLASS_DATA	IRIS
 
 /* Number of times to loop over the samples */
-const static auto EPOCHS = 100;
+const static auto EPOCHS = 10;
 
-/* The number of samples in the dataset */
-const static auto SAMPLES = 3;
+#if CLASS_DATA
+const static auto DATA_ROWS = 4;
+const static auto DATA_COLS = 1;
+const static auto OUTPUT_CLASSES = 3;
 
+const auto FILENAME = "..\\test_data\\iris\\irisMod.csv";
+#else
 const static auto DATA_ROWS = 28;
 const static auto DATA_COLS = 28;
+const static auto OUTPUT_CLASSES = 10;
+
+const auto FILENAME = "..\\test_data\\MNIST\\mnist_test.csv";
+#endif
+
 const static auto LAYER_MULT = 2;
 const static auto IN_SIZE = DATA_ROWS * DATA_COLS;
 const static auto OUT_SIZE = IN_SIZE * LAYER_MULT;
-const static auto OUTPUT_CLASSES = 10;
 const static auto COLS = 1;
 
 double error_den = 1;
 
-#ifdef _USEREALDATA
-const auto FILENAME = "C:\\Users\\Luke\\Documents\\OneDrive\\Documents\\Visual Studio\\Python\\Machine-Learning\\Test-Data\\MNIST\\mnist_test.csv";
-#endif
-
 int main()
 {
-#ifdef _USEREALDATA
-	std::vector<std::vector<type_t>> data = file::parseCSV2(FILENAME);
-#endif
+	/* The number of samples in the dataset */
+	static auto sample_size = 10;
+
+	std::vector<std::vector<type_t>> data = file<type_t>::parseCSV2(FILENAME);
+	sample_size = data.size();
+
 	std::random_device rd;
 #ifdef _USE_FIXED_RAND
 	/* Always generate different random numbers */
@@ -38,6 +54,7 @@ int main()
 	/* Always generate the same random numbers */
 	std::mt19937 gen(rd());
 #endif
+	std::uniform_real_distribution<type_t> distReal(0, 1);
 	std::uniform_int_distribution<> distInt(0, 255);
 	std::uniform_int_distribution<> distBin(0, 1);
 
@@ -45,23 +62,18 @@ int main()
 
 	const auto x_rows = IN_SIZE;
 	const auto x_cols = COLS;
-	const auto x_size = x_rows * x_cols;
-	std::vector<type_t> x;
-	for (int i = 1; i < x_size + 1; ++i)
-	{
-		x.push_back(distInt(gen));
-	}
 
 	const auto error_out_rows = OUTPUT_CLASSES;
 	const auto error_out_cols = COLS;
 	const auto error_out_size = error_out_rows * error_out_cols;
 	std::vector<type_t> error_out(error_out_size, 0);
+	concurrency::array_view<type_t, 2> ar_error_out(error_out_rows, error_out_cols, error_out);
 
 	const auto t_rows = OUTPUT_CLASSES;
 	const auto t_cols = COLS;
 	const auto t_size = t_rows * t_cols;
 	std::vector<type_t> t;
-	for (int i = 0; i < t_size; i++)
+	for (int i = 0; i < t_size; ++i)
 	{
 		t.push_back(distBin(gen));
 	}
@@ -88,30 +100,16 @@ int main()
 		one_hot.push_back(one_hot_row);
 	}
 
-	const auto NEURON_COUNT = 3;
+	logistic_neuron<type_t> neuron_in(OUT_SIZE, IN_SIZE);
+	//logistic_neuron<type_t> neuron_int0(OUT_SIZE, OUT_SIZE);
+	logistic_neuron<type_t> neuron_out(OUTPUT_CLASSES, OUT_SIZE);
 
-	neuron neuron_in(OUT_SIZE, IN_SIZE);
-	neuron neuron_int0(OUT_SIZE, OUT_SIZE);
-	/*neuron neuron_int1(OUT_SIZE, OUT_SIZE);
-	neuron neuron_int2(OUT_SIZE, OUT_SIZE);
-	neuron neuron_int3(OUT_SIZE, OUT_SIZE);
-	neuron neuron_int4(OUT_SIZE, OUT_SIZE);
-	neuron neuron_int5(OUT_SIZE, OUT_SIZE);
-	neuron neuron_int6(OUT_SIZE, OUT_SIZE);
-	neuron neuron_int7(OUT_SIZE, OUT_SIZE);*/
-	neuron neuron_out(OUTPUT_CLASSES, OUT_SIZE);
-
-	std::vector<neuron*> neurons;
+	std::vector<neuron<type_t>*> neurons;
 	neurons.push_back(&neuron_in);
-	neurons.push_back(&neuron_int0);
-	/*neurons.push_back(&neuron_int1);
-	neurons.push_back(&neuron_int2);
-	neurons.push_back(&neuron_int3);
-	neurons.push_back(&neuron_int4);
-	neurons.push_back(&neuron_int5);
-	neurons.push_back(&neuron_int6);
-	neurons.push_back(&neuron_int7);*/
+	//neurons.push_back(&neuron_int0);
 	neurons.push_back(&neuron_out);
+
+	int neuron_count = (neurons.size() < 0) ? 0 : neurons.size();
 
 	auto total_error = 1.0;
 
@@ -119,44 +117,35 @@ int main()
 
 	for (auto epochs = 0; epochs < EPOCHS; ++epochs)
 	{
-		for (auto samples = 0; samples < SAMPLES; ++samples)
+		for (auto samples = 0; samples < sample_size; ++samples)
 		{
-#ifdef _USEREALDATA
+#if CLASS_DATA
+			std::vector<type_t>::const_iterator first = data[samples].begin();
+			std::vector<type_t>::const_iterator last = data[samples].end() - 1;
+#else
 			std::vector<type_t>::const_iterator first = data[samples].begin() + 1;
 			std::vector<type_t>::const_iterator last = data[samples].end();
-			std::vector<type_t> x(first, last);
 #endif
+			std::vector<type_t> x(first, last);
+#ifdef _ARRAYS			
+			concurrency::array_view<type_t, 2> ar_x(x_rows, x_cols, x);
 
+			/* Forward Propogation Step */
 			neurons[0]->fwd(x);
-			for (auto neur_it = 1; neur_it < NEURON_COUNT; ++neur_it)
-			{				
+			for (auto neur_it = 1; neur_it < neuron_count; ++neur_it)
+			{
 				neurons[neur_it]->fwd(neurons[neur_it - 1]->get_y());
 			}
 
-#ifdef _USEREALDATA
-			t = one_hot[data[samples][0]];
-#else
-			t = one_hot[7];
-#endif
-			neuron::matrix_sub(neuron_out.get_y(), t, error_out, error_out_rows, error_out_cols);
+			t = one_hot[data[samples][CLASS_DATA]];
+			concurrency::array_view<type_t, 2> ar_t(t_rows, t_cols, t);
 
-			neurons[NEURON_COUNT - 1]->bkwd(error_out);
-			for (auto neur_it = NEURON_COUNT - 2; neur_it >= 0; --neur_it)
-			{
-				neurons[neur_it]->bkwd(neurons[neur_it + 1]->get_error());
-				neurons[neur_it]->set_error();
-			}
-
-			for (auto neur_it = NEURON_COUNT - 1; neur_it > 0; --neur_it)
-			{
-				neurons[neur_it]->accm(neurons[neur_it-1]->get_y());
-			}
-			neurons[0]->accm(x);
-
-			if (samples % ((EPOCHS * SAMPLES) / 10) == 0)
+			/* Error */
+			nnet_math<type_t>::matrix_sub(neuron_out.get_y(), t, error_out, error_out_rows, error_out_cols);
+			if ((epochs % ((EPOCHS) / 10) == 0) && (samples == 0))
 			{
 				auto val = 100 * epochs / (double)EPOCHS;
-				std::cout << val << "%" << std::endl;
+				ar_error_out.synchronize();
 
 				for (int e = 0; e < error_out_rows; ++e)
 				{
@@ -165,18 +154,90 @@ int main()
 
 				total_error = error_den / OUTPUT_CLASSES;
 
-				std::cout << total_error << std::endl;
+				std::cout << "Progress:" << val << "%	Error:" << total_error << std::endl;
 
 				error_den = 0;
 			}
-		}
 
-		for (auto neur_it = NEURON_COUNT - 1; neur_it >= 0; --neur_it)
+			/* Back Propogation Step */
+			neurons[neuron_count - 1]->bkwd(error_out);
+			neurons[neuron_count - 1]->set_error();
+			for (auto neur_it = neuron_count - 2; neur_it >= 0; --neur_it)
+			{
+				neurons[neur_it]->bkwd(neurons[neur_it + 1]->get_error());
+				neurons[neur_it]->set_error();
+			}
+
+			/* Accumulate Error Step */
+			for (auto neur_it = neuron_count - 1; neur_it > 0; --neur_it)
+			{
+				neurons[neur_it]->accm(neurons[neur_it - 1]->get_y());
+			}
+			neurons[0]->accm(x);
+
+#else
+			concurrency::array_view<type_t, 2> ar_x(x_rows, x_cols, x);
+
+			/* Forward Propogation Step */
+			neurons[0]->fwd(ar_x);
+			for (auto neur_it = 1; neur_it < neuron_count; ++neur_it)
+			{				
+				neurons[neur_it]->fwd(neurons[neur_it - 1]->get_ar_y());
+			}
+
+			t = one_hot[data[samples][CLASS_DATA]];
+			concurrency::array_view<type_t, 2> ar_t(t_rows, t_cols, t);
+
+			/* Error */
+			nnet_math<type_t>::matrix_sub(neuron_out.get_ar_y(), ar_t, ar_error_out);
+			if ((epochs % ((EPOCHS) / 10) == 0) && (samples == 0))
+			{
+				auto val = 100 * epochs / (double)EPOCHS;
+				ar_error_out.synchronize();
+
+				for (int e = 0; e < error_out_rows; ++e)
+				{
+					error_den += abs(error_out[e]);
+				}
+
+				total_error = error_den / OUTPUT_CLASSES;
+
+				std::cout << "Progress:" << val << "%	Error:" << total_error  << std::endl;
+
+				error_den = 0;
+			}
+
+			/* Back Propogation Step */
+			neurons[neuron_count - 1]->bkwd(ar_error_out);
+			neurons[neuron_count - 1]->set_error();
+			for (auto neur_it = neuron_count - 2; neur_it >= 0; --neur_it)
+			{
+				neurons[neur_it]->bkwd(neurons[neur_it + 1]->get_ar_error());
+				neurons[neur_it]->set_error();
+			}
+
+			/* Accumulate Error Step */
+			for (auto neur_it = neuron_count - 1; neur_it > 0; --neur_it)
+			{
+				neurons[neur_it]->accm(neurons[neur_it-1]->get_ar_y());
+			}
+			neurons[0]->accm(ar_x);
+#endif
+		}
+#ifdef _ARRAYS
+		/* Update Weights */
+		for (auto neur_it = neuron_count - 1; neur_it >= 0; --neur_it)
 		{
-			neurons[neur_it]->updt();
+			neurons[neur_it]->updt(sample_size);
 		}
+#else
+		/* Update Weights */
+		for (auto neur_it = neuron_count - 1; neur_it >= 0; --neur_it)
+		{
+			neurons[neur_it]->updt(sample_size);
+		}
+#endif
 	}
-
 	std::chrono::steady_clock::time_point end_time(std::chrono::steady_clock::now());
 
 	std::cout << std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time).count();
