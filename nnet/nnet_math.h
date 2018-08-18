314679faaf4fe2af8b93d129d16dd0a9eaf1ea0d
Luke Lorenzini <luke.lorenzini@gmail.com>
2018-08-18 21:22:34 +0100

    Remove old code

--- nnet/nnet_math.h
+++ nnet/nnet_math.h
@@ -12,54 +12,22 @@ private:
 	static const int RANK = 2;
 
 public:
-	static void matrix_mult(const std::vector<type_t> &a, const std::vector<type_t> &b, std::vector<type_t> &res, int M, int N, int W);
 	static void matrix_mult(concurrency::array_view<type_t, RANK> &ar_a, concurrency::array_view<type_t, RANK> &ar_b, concurrency::array_view<type_t, RANK> &ar_res);
 	static void matrix_mult_tile(concurrency::array_view<type_t, RANK> &ar_a, concurrency::array_view<type_t, RANK> &ar_b, concurrency::array_view<type_t, RANK> &ar_res);
-	static void matrix_sub(const std::vector<type_t> &a, const std::vector<type_t> &b, std::vector<type_t> &res, int M, int N);
-	static void scalar_mult(const std::vector<type_t>& a, type_t mult, std::vector<type_t>& res, int M, int N);
 	static void scalar_mult(concurrency::array_view<type_t, RANK>& ar_a, type_t mult, concurrency::array_view<type_t, RANK>& ar_res);
-	static void scalar_div(const std::vector<type_t>& a, type_t div, std::vector<type_t>& res, int M, int N);
 	static void scalar_div(concurrency::array_view<type_t, RANK>& ar_a, type_t div, concurrency::array_view<type_t, RANK>& ar_res);
 	static void matrix_sub(concurrency::array_view<type_t, RANK>& ar_a, concurrency::array_view<type_t, RANK>& ar_b, concurrency::array_view<type_t, RANK>& ar_res);
-	static void matrix_add(const std::vector<type_t>& a, const std::vector<type_t>& b, std::vector<type_t>& res, int M, int N);
 	static void logistic(concurrency::array_view<type_t, RANK>& ar_a, concurrency::array_view<type_t, RANK>& ar_res);
-	static void tanh(const std::vector<type_t>& a, std::vector<type_t>& res, int M, int N);
 	static void tanh(concurrency::array_view<type_t, RANK>& ar_a, concurrency::array_view<type_t, RANK>& ar_res);
-	static void relu(const std::vector<type_t>& a, std::vector<type_t>& res, int M, int N);
 	static void relu(concurrency::array_view<type_t, RANK>& ar_a, concurrency::array_view<type_t, RANK>& ar_res);
-	static void relu_der(const std::vector<type_t>& a, std::vector<type_t>& res, int M, int N);
 	static void relu_der(concurrency::array_view<type_t, RANK>& ar_a, concurrency::array_view<type_t, RANK>& ar_res);
 	static void matrix_add(concurrency::array_view<type_t, RANK>& ar_a, concurrency::array_view<type_t, RANK>& ar_b, concurrency::array_view<type_t, RANK>& ar_res);
-	static void matrix_prod(const std::vector<type_t>& a, const std::vector<type_t>& b, std::vector<type_t>& res, int M, int N);
 	static void matrix_prod(concurrency::array_view<type_t, RANK>& ar_a, concurrency::array_view<type_t, RANK>& ar_b, concurrency::array_view<type_t, RANK>& ar_res);
-	static void matrix_trans(const std::vector<type_t>& a, std::vector<type_t>& res, int M, int N);
 	static void matrix_trans(concurrency::array_view<type_t, RANK>& ar_a, concurrency::array_view<type_t, RANK>& ar_res);
-	static void logistic(const std::vector<type_t>& a, std::vector<type_t>& res, int M, int N);
 	static void exponent(concurrency::array_view<type_t, RANK> &ar_a, concurrency::array_view<type_t, RANK> &ar_res);
 	static void softmax(concurrency::array_view<type_t, RANK> &ar_a, type_t sum, concurrency::array_view<type_t, RANK> &ar_res);
 	static void softmax_der(concurrency::array_view<type_t, RANK> &ar_a, concurrency::array_view<type_t, RANK> &ar_res);
 };
-
-template<class type_t>
-inline void nnet_math<type_t>::matrix_mult(const std::vector<type_t>& a, const std::vector<type_t>& b, std::vector<type_t>& res, int M, int N, int W)
-{
-	concurrency::array_view<const type_t, RANK> ar_a(M, N, a);
-	concurrency::array_view<const type_t, RANK> ar_b(N, W, b);
-	concurrency::array_view<type_t, RANK> ar_res(M, W, res);
-	//ar_res.discard_data();
-
-	parallel_for_each(ar_res.extent, [=](concurrency::index<RANK> idx) restrict(amp)
-	{
-		auto row = idx[0];
-		auto col = idx[1];
-
-		for (auto inner = 0; inner < RANK; inner++) {
-			ar_res[idx] += ar_a(row, inner) * ar_b(inner, col);
-		}
-	});
-
-	ar_res.synchronize();
-}
 
 template<class type_t>
 inline void nnet_math<type_t>::matrix_mult(concurrency::array_view<type_t, RANK>& ar_a, concurrency::array_view<type_t, RANK>& ar_b, concurrency::array_view<type_t, RANK>& ar_res)
@@ -109,43 +77,6 @@ inline void nnet_math<type_t>::matrix_mult_tile(concurrency::array_view<type_t, 
 }
 
 template<class type_t>
-inline void nnet_math<type_t>::matrix_sub(const std::vector<type_t>& a, const std::vector<type_t>& b, std::vector<type_t>& res, int M, int N)
-{
-	concurrency::array_view<const type_t, RANK> ar_a(M, N, a);
-	concurrency::array_view<const type_t, RANK> ar_b(M, N, b);
-	concurrency::array_view<type_t, RANK> ar_res(M, N, res);
-	//ar_res.discard_data();
-
-	parallel_for_each(ar_res.extent, [=](concurrency::index<RANK> idx) restrict(amp)
-	{
-		auto row = idx[0];
-		auto col = idx[1];
-
-		ar_res[row][col] = ar_a[row][col] - ar_b[row][col];
-	});
-
-	ar_res.synchronize();
-}
-
-template<class type_t>
-inline void nnet_math<type_t>::scalar_mult(const std::vector<type_t> &a, type_t mult, std::vector<type_t> &res, int M, int N)
-{
-	concurrency::array_view<const type_t, RANK> ar_a(M, N, a);
-	concurrency::array_view<type_t, RANK> ar_res(M, N, res);
-	//ar_res.discard_data();
-
-	parallel_for_each(ar_res.extent, [=](concurrency::index<RANK> idx) restrict(amp)
-	{
-		auto row = idx[0];
-		auto col = idx[1];
-
-		ar_res[row][col] = ar_a[row][col] * mult;
-	});
-
-	ar_res.synchronize();
-}
-
-template<class type_t>
 inline void nnet_math<type_t>::scalar_mult(concurrency::array_view<type_t, RANK> &ar_a, type_t mult, concurrency::array_view<type_t, RANK> &ar_res)
 {
 #ifdef DISCARD
@@ -161,24 +92,6 @@ inline void nnet_math<type_t>::scalar_mult(concurrency::array_view<type_t, RANK>
 #ifdef SYNC
 	ar_res.synchronize();
 #endif
-}
-
-template<class type_t>
-inline void nnet_math<type_t>::scalar_div(const std::vector<type_t> &a, type_t div, std::vector<type_t> &res, int M, int N)
-{
-	concurrency::array_view<const type_t, RANK> ar_a(M, N, a);
-	concurrency::array_view<type_t, RANK> ar_res(M, N, res);
-	//ar_res.discard_data();
-
-	parallel_for_each(ar_res.extent, [=](concurrency::index<RANK> idx) restrict(amp)
-	{
-		auto row = idx[0];
-		auto col = idx[1];
-
-		ar_res[row][col] = ar_a[row][col] / div;
-	});
-
-	ar_res.synchronize();
 }
 
 template<class type_t>
@@ -218,25 +131,6 @@ inline void nnet_math<type_t>::matrix_sub(concurrency::array_view<type_t, RANK> 
 }
 
 template<class type_t>
-inline void nnet_math<type_t>::matrix_add(const std::vector<type_t> &a, const std::vector<type_t> &b, std::vector<type_t> &res, int M, int N)
-{
-	concurrency::array_view<const type_t, RANK> ar_a(M, N, a);
-	concurrency::array_view<const type_t, RANK> ar_b(M, N, b);
-	concurrency::array_view<type_t, RANK> ar_res(M, N, res);
-	//ar_res.discard_data();
-
-	parallel_for_each(ar_res.extent, [=](concurrency::index<RANK> idx) restrict(amp)
-	{
-		auto row = idx[0];
-		auto col = idx[1];
-
-		ar_res[row][col] = ar_a[row][col] + ar_b[row][col];
-	});
-
-	ar_res.synchronize();
-}
-
-template<class type_t>
 inline void nnet_math<type_t>::matrix_add(concurrency::array_view< type_t, RANK> &ar_a, concurrency::array_view<type_t, RANK> &ar_b, concurrency::array_view<type_t, RANK> &ar_res)
 {
 #ifdef DISCARD
@@ -252,25 +146,6 @@ inline void nnet_math<type_t>::matrix_add(concurrency::array_view< type_t, RANK>
 #ifdef SYNC
 	ar_res.synchronize();
 #endif
-}
-
-template<class type_t>
-inline void nnet_math<type_t>::matrix_prod(const std::vector<type_t> &a, const std::vector<type_t> &b, std::vector<type_t> &res, int M, int N)
-{
-	concurrency::array_view<const type_t, RANK> ar_a(M, N, a);
-	concurrency::array_view<const type_t, RANK> ar_b(M, N, b);
-	concurrency::array_view<type_t, RANK> ar_res(M, N, res);
-	//ar_res.discard_data();
-
-	parallel_for_each(ar_res.extent, [=](concurrency::index<RANK> idx) restrict(amp)
-	{
-		auto row = idx[0];
-		auto col = idx[1];
-
-		ar_res[row][col] = ar_a[row][col] * ar_b[row][col];
-	});
-
-	ar_res.synchronize();
 }
 
 template<class type_t>
@@ -292,25 +167,6 @@ inline void nnet_math<type_t>::matrix_prod(concurrency::array_view<type_t, RANK>
 }
 
 template<class type_t>
-inline void nnet_math<type_t>::matrix_trans(const std::vector<type_t> &a, std::vector<type_t> &res, int M, int N)
-{
-	concurrency::array_view<const type_t, RANK> ar_a(M, N, a);
-	/* Dimensions intentionally changed for transpose */
-	concurrency::array_view<type_t, RANK> ar_res(N, M, res);
-	//ar_res.discard_data();
-
-	parallel_for_each(ar_res.extent, [=](concurrency::index<RANK> idx) restrict(amp)
-	{
-		auto row = idx[0];
-		auto col = idx[1];
-
-		ar_res[row][col] = ar_a[col][row];
-	});
-
-	ar_res.synchronize();
-}
-
-template<class type_t>
 inline void nnet_math<type_t>::matrix_trans(concurrency::array_view<type_t, RANK> &ar_a, concurrency::array_view<type_t, RANK> &ar_res)
 {
 #ifdef DISCARD
@@ -326,24 +182,6 @@ inline void nnet_math<type_t>::matrix_trans(concurrency::array_view<type_t, RANK
 #ifdef SYNC
 	ar_res.synchronize();
 #endif
-}
-
-template<class type_t>
-inline void nnet_math<type_t>::logistic(const std::vector<type_t> &a, std::vector<type_t> &res, int M, int N)
-{
-	concurrency::array_view<const type_t, RANK> ar_a(M, N, a);
-	concurrency::array_view<type_t, RANK> ar_res(M, N, res);
-	//ar_res.discard_data();
-
-	parallel_for_each(ar_res.extent, [=](concurrency::index<RANK> idx) restrict(amp)
-	{
-		auto row = idx[0];
-		auto col = idx[1];
-
-		ar_res[row][col] = 1 / (1 + concurrency::precise_math::exp(-1 * ar_a[row][col]));
-	});
-
-	ar_res.synchronize();
 }
 
 template<class type_t>
@@ -365,24 +203,6 @@ inline void nnet_math<type_t>::logistic(concurrency::array_view<type_t, RANK> &a
 }
 
 template<class type_t>
-inline void nnet_math<type_t>::tanh(const std::vector<type_t> &a, std::vector<type_t> &res, int M, int N)
-{
-	concurrency::array_view<const type_t, RANK> ar_a(M, N, a);
-	concurrency::array_view<type_t, RANK> ar_res(M, N, res);
-	//ar_res.discard_data();
-
-	parallel_for_each(ar_res.extent, [=](concurrency::index<RANK> idx) restrict(amp)
-	{
-		auto row = idx[0];
-		auto col = idx[1];
-
-		ar_res[row][col] = concurrency::precise_math::tanh(ar_a[row][col]);
-	});
-
-	ar_res.synchronize();
-}
-
-template<class type_t>
 inline void nnet_math<type_t>::tanh(concurrency::array_view<type_t, RANK> &ar_a, concurrency::array_view<type_t, RANK> &ar_res)
 {
 #ifdef DISCARD
@@ -398,31 +218,6 @@ inline void nnet_math<type_t>::tanh(concurrency::array_view<type_t, RANK> &ar_a,
 #ifdef SYNC
 	ar_res.synchronize();
 #endif
-}
-
-template<class type_t>
-inline void nnet_math<type_t>::relu(const std::vector<type_t> &a, std::vector<type_t> &res, int M, int N)
-{
-	concurrency::array_view<const type_t, RANK> ar_a(M, N, a);
-	concurrency::array_view<type_t, RANK> ar_res(M, N, res);
-	//ar_res.discard_data();
-
-	parallel_for_each(ar_res.extent, [=](concurrency::index<RANK> idx) restrict(amp)
-	{
-		auto row = idx[0];
-		auto col = idx[1];
-
-		if (ar_a[row][col] < 0)
-		{
-			ar_res[row][col] = 0;
-		}
-		else
-		{
-			ar_res[row][col] = ar_a[row][col];
-		}
-	});
-
-	ar_res.synchronize();
 }
 
 template<class type_t>
@@ -448,31 +243,6 @@ inline void nnet_math<type_t>::relu(concurrency::array_view<type_t, RANK> &ar_a,
 #ifdef SYNC
 	ar_res.synchronize();
 #endif
-}
-
-template<class type_t>
-inline void nnet_math<type_t>::relu_der(const std::vector<type_t> &a, std::vector<type_t> &res, int M, int N)
-{
-	concurrency::array_view<const type_t, RANK> ar_a(M, N, a);
-	concurrency::array_view<type_t, RANK> ar_res(M, N, res);
-	//ar_res.discard_data();
-
-	parallel_for_each(ar_res.extent, [=](concurrency::index<RANK> idx) restrict(amp)
-	{
-		auto row = idx[0];
-		auto col = idx[1];
-
-		if (ar_a[row][col] < 0)
-		{
-			ar_res[row][col] = 0;
-		}
-		else
-		{
-			ar_res[row][col] = 1;
-		}
-	});
-
-	ar_res.synchronize();
 }
 
 template<class type_t>
