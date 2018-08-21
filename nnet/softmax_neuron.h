fadcecd552070cced87e2e3e80f1239fa78da151
Luke Lorenzini <luke.lorenzini@gmail.com>
2018-08-21 14:20:06 +0100

    Naive Softmax

--- nnet/softmax_neuron.h
+++ nnet/softmax_neuron.h
@@ -38,7 +38,34 @@ void softmax_neuron<type_t>::activate()
 template <class type_t>
 void softmax_neuron<type_t>::activate_der()
 {
-	nnet_math<type_t>::matrix_sub(ar_ones, ar_y, ar_t_y);
+	/*
+	https://stackoverflow.com/questions/33541930/how-to-implement-the-softmax-derivative-independently-from-any-loss-function
+	https://stats.stackexchange.com/questions/79454/softmax-layer-in-a-neural-network
+	*/
+	std::vector<type_t> Jacobian;
 
-	nnet_math<type_t>::matrix_prod(ar_y, ar_t_y, ar_t_e0);
+	// Jacobian
+	for (auto i = 0; i < ar_y.extent[0]; ++i)
+	{
+		for (auto j = 0; j < ar_y.extent[0]; ++j)
+		{
+			if (i == j)
+			{
+				Jacobian.push_back(y[i] * (1 - y[i]));
+			}
+			else
+			{
+				Jacobian.push_back(-y[i] * y[j]);
+			}
+		}
+	}
+
+	// Jacobian * dy (derrivative of cost function)
+	concurrency::array_view<type_t, RANK> ar_Jacobian(3, 3, Jacobian);
+
+#ifdef _USE_TILES
+	nnet_math<type_t>::matrix_mult_tile(ar_Jacobian, ar_y, ar_t_e0);
+#else
+	nnet_math<type_t>::matrix_mult(ar_Jacobian, ar_y, ar_t_e0);
+#endif
 }
