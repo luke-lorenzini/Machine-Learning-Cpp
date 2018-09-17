b78bad3db500af5a4a02c6804f7539044ab22946
Luke Lorenzini <luke.lorenzini@gmail.com>
2018-09-17 15:00:24 +0200

    Update verification step

--- nnet/nnet.cpp
+++ nnet/nnet.cpp
@@ -372,9 +372,6 @@ void nnet::run_parallel(input_data &data)
 
 void nnet::verify(input_data& data)
 {
-	std::vector<type_t> error_out(error_out_size, 0);
-	concurrency::array_view<type_t, 2> ar_error_out(error_out_rows, error_out_cols, error_out);
-
 	/* Verify */
 	const auto CHECK0 = 0;
 	const auto CHECK1 = 50;
@@ -383,28 +380,30 @@ void nnet::verify(input_data& data)
 	/* Check 0 */
 	concurrency::array_view<type_t, 2> x_check0(x_rows, x_cols, data.x[CHECK0]);
 	concurrency::array_view<type_t, 2> t_check0(t_rows, t_cols, data.t[CHECK0]);
-	concurrency::array_view<type_t, 2> error_check0(error_out_rows, error_out_cols, error_out);
-
+	std::vector<type_t> error0(error_out_size, 0);
+	concurrency::array_view<type_t, 2> error_check0(error_out_rows, error_out_cols, error0);
 	neurons[0]->check(x_check0);
 	neurons[1]->check(neurons[0]->get_ar_y());
+	/* If error_check0 is all 0's, training was correct for sample */
 	nnet_math<type_t>::matrix_sub(neurons[1]->get_ar_y(), t_check0, error_check0);
 
 	/* Check 1*/
 	concurrency::array_view<type_t, 2> x_check1(x_rows, x_cols, data.x[CHECK1]);
 	concurrency::array_view<type_t, 2> t_check1(t_rows, t_cols, data.t[CHECK1]);
-	concurrency::array_view<type_t, 2> error_check1(error_out_rows, error_out_cols, error_out);
-
+	std::vector<type_t> error1(error_out_size, 0);
+	concurrency::array_view<type_t, 2> error_check1(error_out_rows, error_out_cols, error1);
 	neurons[0]->check(x_check1);
 	neurons[1]->check(neurons[0]->get_ar_y());
+	/* If error_check0 is all 1's, training was correct for sample */
 	nnet_math<type_t>::matrix_sub(neurons[1]->get_ar_y(), t_check1, error_check1);
 
 	/* Check 2*/
 	concurrency::array_view<type_t, 2> x_check2(x_rows, x_cols, data.x[CHECK2]);
 	concurrency::array_view<type_t, 2> t_check2(t_rows, t_cols, data.t[CHECK2]);
-	concurrency::array_view<type_t, 2> error_check2(error_out_rows, error_out_cols, error_out);
-
+	std::vector<type_t> error2(error_out_size, 0);
+	concurrency::array_view<type_t, 2> error_check2(error_out_rows, error_out_cols, error2);
 	neurons[0]->check(x_check2);
 	neurons[1]->check(neurons[0]->get_ar_y());
+	/* If error_check2 is all 0's, training was correct for sample */
 	nnet_math<type_t>::matrix_sub(neurons[1]->get_ar_y(), t_check2, error_check2);
 }
-
