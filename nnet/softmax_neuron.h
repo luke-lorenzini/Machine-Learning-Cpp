0b61e54eb962091102c07150667640f8ddd741eb
Luke Lorenzini <luke.lorenzini@gmail.com>
2018-09-27 18:24:24 +0200

    Extract max value from z array, not y in softmax. Block out softmax derivative since not being used (yet).

--- nnet/softmax_neuron.h
+++ nnet/softmax_neuron.h
@@ -32,19 +32,21 @@ softmax_neuron<type_t>::~softmax_neuron()
 template <class type_t>
 void softmax_neuron<type_t>::activate()
 {
-	auto maxi = std::max_element(std::begin(t_y), std::end(t_y));
+	// Extract the maxium value and convert to type type_t
+	auto maxi = std::max_element(std::begin(z), std::end(z));
 	type_t mmm = *maxi;
 
 	nnet_math<type_t>::exponent(ar_z, ar_t_y, -mmm);
 
 	type_t sum = concurrency::parallel_reduce(begin(t_y), end(t_y), 0, std::plus<type_t>());
 
-	nnet_math<type_t>::softmax(ar_t_y, (sum + 0.00001), ar_y);
+	nnet_math<type_t>::softmax(ar_t_y, sum, ar_y);
 }
 
 template <class type_t>
 void softmax_neuron<type_t>::activate_der()
 {
+#if 0
 	/*
 	https://stackoverflow.com/questions/33541930/how-to-implement-the-softmax-derivative-independently-from-any-loss-function
 	https://stats.stackexchange.com/questions/79454/softmax-layer-in-a-neural-network
@@ -74,5 +76,9 @@ void softmax_neuron<type_t>::activate_der()
 	nnet_math<type_t>::matrix_mult_tile(ar_Jacobian, ar_y, ar_t_e0);
 #else
 	nnet_math<type_t>::matrix_mult(ar_Jacobian, ar_y, ar_t_e0);
+#endif
+#else
+	//ar_t_e0 = ar_ones;
+	concurrency::copy(ar_ones, ar_t_e0);
 #endif
 }
