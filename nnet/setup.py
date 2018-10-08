799c292959b318688dc6a789df106f60fe5386ed
Luke Lorenzini <luke.lorenzini@gmail.com>
2018-10-08 11:33:02 +0200

    Add setup.py file

---
+++ nnet/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup, Extension, DEBUG
+
+sfc_module = Extension('nnet', sources = ['source.cpp', 'nnet.cpp', 'gpu.cpp'])
+
+setup(name = 'nnet', version = '1.0',
+    description = 'Python Package with nnet C++ extension',
+    ext_modules = [sfc_module]
+    )
