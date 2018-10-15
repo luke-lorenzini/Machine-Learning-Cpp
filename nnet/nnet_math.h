b9370c15f45eadcad0e6518f3b898e3792e94f5e
Luke Lorenzini <luke.lorenzini@gmail.com>
2018-10-15 13:20:34 +0200

    More work on tiling, still not there

--- nnet/nnet_math.h
+++ nnet/nnet_math.h
@@ -14,10 +14,13 @@ private:
 	nnet_math() {}
 
 	static const int RANK = 2;
+	static const int TILE_SIZE = 8;
 
 public:
 	static void matrix_mult(concurrency::array_view<type_t, RANK> &ar_a, concurrency::array_view<type_t, RANK> &ar_b, concurrency::array_view<type_t, RANK> &ar_res);
 	static void matrix_mult_tile(concurrency::array_view<type_t, RANK> &ar_a, concurrency::array_view<type_t, RANK> &ar_b, concurrency::array_view<type_t, RANK> &ar_res);
+	static type_t PaddedRead(const concurrency::array_view<const type_t, RANK> &A, concurrency::index<RANK> idx) restrict(amp);
+	static void PaddedWrite(const concurrency::array_view<type_t, RANK> &A, const concurrency::index<RANK> &idx, const type_t &val) restrict(amp);
 	static void scalar_mult(concurrency::array_view<type_t, RANK>& ar_a, type_t mult, concurrency::array_view<type_t, RANK>& ar_res);
 	static void scalar_div(concurrency::array_view<type_t, RANK>& ar_a, type_t div, concurrency::array_view<type_t, RANK>& ar_res);
 	static void matrix_sub(concurrency::array_view<type_t, RANK>& ar_a, concurrency::array_view<type_t, RANK>& ar_b, concurrency::array_view<type_t, RANK>& ar_res);
@@ -60,27 +63,36 @@ inline void nnet_math<type_t>::matrix_mult(concurrency::array_view<type_t, RANK>
 template<class type_t>
 inline void nnet_math<type_t>::matrix_mult_tile(concurrency::array_view<type_t, RANK>& ar_a, concurrency::array_view<type_t, RANK>& ar_b, concurrency::array_view<type_t, RANK>& ar_res)
 {
-	static const int TileSize = 1;
-
 #ifdef DISCARD
 	ar_res.discard_data();
 #endif
-	parallel_for_each(ar_res.extent.tile<TileSize, TileSize>(), [=](concurrency::tiled_index<TileSize, TileSize> tidx) restrict(amp)
-	{
+
+	concurrency::tiled_extent<TILE_SIZE, TILE_SIZE> computeDomain = ar_res.extent.tile<TILE_SIZE, TILE_SIZE>();
+	computeDomain = computeDomain.pad();
+
+	parallel_for_each(computeDomain, [=](concurrency::tiled_index<TILE_SIZE, TILE_SIZE> tidx) restrict(amp)
+	{		
 		auto row = tidx.global[0];
 		auto col = tidx.global[1];
 		type_t sum = 0.0;
 
-		for (auto i = 0; i < ar_a.extent[1]; i += TileSize)
+		for (auto i = 0; i < ar_a.extent[1]; i += TILE_SIZE)
 		{
-			tile_static type_t sA[TileSize][TileSize];
-			tile_static type_t sB[TileSize][TileSize];
-			sA[row][col] = ar_a(tidx.global[0], col + i);
+			tile_static type_t sA[TILE_SIZE][TILE_SIZE];
+			tile_static type_t sB[TILE_SIZE][TILE_SIZE];
+#if 0
+			sA[row][col] = ar_a(tidx.global[0], col + i);			
 			sB[row][col] = ar_b(row + i, tidx.global[1]);
+#else
+			//sA[row][col] = ar_a.extent.contains(tidx.global) ? ar_a(tidx.global[0], col + i) : type_t();
+			//sB[row][col] = ar_b.extent.contains(tidx.global) ? ar_b(row + i, tidx.global[1]) : type_t();	
+			sA[row][col] = PaddedRead(ar_a, tidx.global);
+			sB[row][col] = PaddedRead(ar_b, tidx.global);
+#endif
 
 			tidx.barrier.wait();
 
-			for (auto k = 0; k < TileSize; k++)
+			for (auto k = 0; k < TILE_SIZE; k++)
 			{
 				sum += sA[row][k] * sB[k][col];
 			}
@@ -93,6 +105,19 @@ inline void nnet_math<type_t>::matrix_mult_tile(concurrency::array_view<type_t, 
 #ifdef SYNC
 	ar_res.synchronize();
 #endif	
+}
+
+template<class type_t>
+inline type_t nnet_math<type_t>::PaddedRead(const concurrency::array_view<const type_t, RANK>& A, const concurrency::index<RANK> idx) restrict(amp)
+{
+	return A.extent.contains(idx) ? A[idx] : type_t();
+}
+
+template<class type_t>
+inline void nnet_math<type_t>::PaddedWrite(const concurrency::array_view<type_t, RANK>& A, const concurrency::index<RANK>& idx, const type_t & val) restrict(amp)
+{
+	if (A.extent.contains(idx))
+		A[idx] = val;
 }
 
 template<class type_t>
